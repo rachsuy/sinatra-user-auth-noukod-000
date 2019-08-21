28054b616b3fc40065d0b73338cf4989e74c2e4c
Rachele Suy <rachelesuy@gmail.com>
2019-08-21 21:09:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,31 +11,45 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
+  # render the sign-up form view.
   get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
+  # responsible for handling the POST request that is sent when a user hits 'submit' on the sign-up form
   post '/registrations' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:user_id] = @user.id
 
     redirect '/users/home'
   end
 
+  # is responsible for rendering the login form
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
+  # This route is responsible for receiving the POST request that gets sent when a user hits 'submit' on the login form.
+  # find the correct user from the database and log them in
   post '/sessions' do
-
-    redirect '/users/home'
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
   end
 
-  get '/sessions/logout' do 
-
+  # This route is responsible for logging the user out by clearing the session hash.
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
+  # This route is responsible for rendering the user's homepage view.
   get '/users/home' do
-
+    @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 
